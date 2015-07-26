3216d382ac1200830cdff3be44e9cfff28f18d85
s-nakajima <nakajimashouhei@gmail.com>
2015-07-26 18:57:17 +0900

    Fix by develop specification of user attributes

---
+++ Model/UserAttributeLayout.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * UserAttributeLayout Model
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('UserAttributesAppModel', 'UserAttributes.Model');
+
+/**
+ * UserAttributeLayout Model
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @package NetCommons\UserAttributes\Model
+ */
+class UserAttributeLayout extends UserAttributesAppModel {
+
+/**
+ * Use database config
+ *
+ * @var string
+ */
+	public $useDbConfig = 'master';
+
+/**
+ * Validation rules
+ *
+ * @var array
+ */
+	public $validate = array(
+		'col' => array(
+			'numeric' => array(
+				'rule' => array('numeric'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+}
