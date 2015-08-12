ea1a456f63d02dbb41477a8122c258cacbec642b
s-nakajima <nakajimashouhei@gmail.com>
2015-08-12 11:58:49 +0900

    phpcs エラーの修正

--- Test/Case/Model/UserAttributeSettingTest.php
+++ Test/Case/Model/UserAttributeSettingTest.php
@@ -2,9 +2,11 @@
 /**
  * UserAttributeSetting Test Case
  *
-* @author Jun Nishikawa <topaz2@m0n0m0n0.com>
-* @link http://www.netcommons.org NetCommons Project
-* @license http://www.netcommons.org/license.txt NetCommons License
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
  */
 
 App::uses('UserAttributeSetting', 'UserAttributes.Model');
