e686d02f5f03ef34fe26d4e4566a8fe9ba0599c4
s-nakajima <nakajimashouhei@gmail.com>
2016-02-03 22:36:05 +0900

    UserAttributeSetting::saveUserAttributeWeight()のテスト

--- Test/Case/Model/UserAttributeSetting/SaveUserAttributeWeightTest.php
+++ Test/Case/Model/UserAttributeSetting/SaveUserAttributeWeightTest.php
@@ -61,16 +61,17 @@ class UserAttributeSettingSaveUserAttributeWeightTest extends NetCommonsModelTes
 		$model = $this->_modelName;
 		$methodName = $this->_methodName;
 
-		//更新前のデータ取得
-//		$expected = $this->$model->find('all', array(
-//			'recursive' => -1,
-//			'order' => array('id' => 'asc'),
-//		));
-//		$expected = Hash::combine($expected, '{n}.UserAttributeSetting.id', '{n}');
-//		$expected = Hash::remove($expected, '5.UserAttributeSetting.modified');
-//		$expected = Hash::remove($expected, '5.UserAttributeSetting.modified_user');
-//		$expected['5']['UserAttributeSetting']['weight'] = '6';
-//		$expected['5']['UserAttributeSetting']['weight'] = '6';
+		//期待値のデータ取得
+		$expected = $this->$model->find('all', array(
+			'recursive' => -1,
+			'order' => array('id' => 'asc'),
+		));
+		$expected = Hash::combine($expected, '{n}.UserAttributeSetting.id', '{n}');
+		$expected = Hash::remove($expected, '5.UserAttributeSetting.modified');
+		$expected = Hash::remove($expected, '5.UserAttributeSetting.modified_user');
+		$expected['5']['UserAttributeSetting']['weight'] = '6';
+		$expected['16']['UserAttributeSetting']['weight'] = '4';
+		$expected['17']['UserAttributeSetting']['weight'] = '5';
 
 		//データ生成
 		$data = array(
@@ -83,21 +84,93 @@ class UserAttributeSettingSaveUserAttributeWeightTest extends NetCommonsModelTes
 		//チェック
 		$this->assertTrue($result);
 
-//		$actual = $this->$model->find('all', array(
-//			'recursive' => -1,
-//			'order' => array('id' => 'asc'),
-//		));
-//		$actual = Hash::combine($beforeData, '{n}.UserAttributeSetting.id', '{n}');
-//		$actual = Hash::remove($beforeData, '5.UserAttributeSetting.modified');
-//		$actual = Hash::remove($beforeData, '5.UserAttributeSetting.modified_user');
+		$actual = $this->$model->find('all', array(
+			'recursive' => -1,
+			'order' => array('id' => 'asc'),
+		));
+		$actual = Hash::combine($actual, '{n}.UserAttributeSetting.id', '{n}');
+		$actual = Hash::remove($actual, '5.UserAttributeSetting.modified');
+		$actual = Hash::remove($actual, '5.UserAttributeSetting.modified_user');
 
-//		$this->assertEquals($result, $actual)
+		$this->assertEquals($expected, $actual);
+	}
 
+/**
+ * データエラーのDataProvider
+ *
+ * ### 戻り値
+ *  - data 登録データ
+ *
+ * @return array テストデータ
+ */
+	public function dataProvider() {
+		return array(
+			// * 存在しないID
+			array(array(
+				'UserAttributeSetting' => array('id' => '999', 'row' => '1', 'col' => '2', 'weight' => '6')
+			)),
+			// * rowが数値以外
+			array(array(
+				'UserAttributeSetting' => array('id' => '5', 'row' => 'aa', 'col' => '2', 'weight' => '6')
+			)),
+			// * rowがない
+			array(array(
+				'UserAttributeSetting' => array('id' => '5', 'col' => '2', 'weight' => '6')
+			)),
+			// * colが数値以外
+			array(array(
+				'UserAttributeSetting' => array('id' => '5', 'row' => '1', 'col' => 'a', 'weight' => '6')
+			)),
+			// * colがない
+			array(array(
+				'UserAttributeSetting' => array('id' => '5', 'row' => '1', 'weight' => '6')
+			)),
+			// * weightが数値以外
+			array(array(
+				'UserAttributeSetting' => array('id' => '5', 'row' => '1', 'col' => '2', 'weight' => 'a')
+			)),
+			// * weightがない
+			array(array(
+				'UserAttributeSetting' => array('id' => '5', 'row' => '1', 'col' => '2')
+			)),
+		);
+	}
 
-//		$actual = $this->$model->find('first', array('recursive' => -1,
-//			'conditions' => array('id' => $data['UserAttributeSetting']['id'])
-//		));
-//		$this->assertEquals($data['UserAttributeSetting']['display'], $actual['UserAttributeSetting']['display']);
+/**
+ * データエラーのテスト
+ *
+ * @param array $data 登録データ
+ * @dataProvider dataProvider
+ * @return void
+ */
+	public function testDataError($data) {
+		//テスト実施
+		$model = $this->_modelName;
+		$methodName = $this->_methodName;
+		$result = $this->$model->$methodName($data);
+
+		//チェック
+		$this->assertFalse($result);
+	}
+
+/**
+ * ErrorExceptionのテスト
+ *
+ * @return void
+ */
+	public function testErrorException() {
+		$this->_mockForReturnFalse('UserAttributeSetting', 'UserAttributes.UserAttributeSetting', 'save');
+
+		$this->setExpectedException('InternalErrorException');
+		//データ生成
+		$data = array(
+			'UserAttributeSetting' => array('id' => '5', 'row' => '1', 'col' => '2', 'weight' => '6')
+		);
+
+		//テスト実施
+		$model = $this->_modelName;
+		$methodName = $this->_methodName;
+		$this->$model->$methodName($data);
 	}
 
 }
