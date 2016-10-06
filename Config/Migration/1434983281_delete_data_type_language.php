82d47a66995d3df1cb4ea9b95913b4fe71797540
s-nakajima <nakajimashouhei@gmail.com>
2016-10-06 15:27:27 +0900

    ファイル名変更

---
+++ Config/Migration/1434983281_delete_data_type_language.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * データタイプから言語タイプ削除 migration
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsMigration', 'NetCommons.Config/Migration');
+
+/**
+ * データタイプから言語タイプ削除 migration
+ *
+ * @package NetCommons\UserAttributes\Config\Migration
+ */
+class DeleteDataTypeLanguage extends NetCommonsMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'delete_data_type_language';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(),
+		'down' => array(),
+	);
+
+/**
+ * Insert records
+ *
+ * @var array $migration
+ */
+	public $records = array(
+		'UserAttributeChoice' => array(
+			array('language_id' => '2', 'user_attribute_id' => '', 'key' => 'language_0', 'name' => '自動', 'code' => 'auto', 'weight' => '1', ),
+			array('language_id' => '2', 'user_attribute_id' => '', 'key' => 'language_1', 'name' => '英語', 'code' => 'en', 'weight' => '2', ),
+			array('language_id' => '2', 'user_attribute_id' => '', 'key' => 'language_2', 'name' => '日本語', 'code' => 'ja', 'weight' => '3', ),
+			array('language_id' => '1', 'user_attribute_id' => '', 'key' => 'language_0', 'name' => 'Auto', 'code' => 'auto', 'weight' => '1', ),
+			array('language_id' => '1', 'user_attribute_id' => '', 'key' => 'language_1', 'name' => 'English', 'code' => 'en', 'weight' => '2', ),
+			array('language_id' => '1', 'user_attribute_id' => '', 'key' => 'language_2', 'name' => 'Japanese', 'code' => 'ja', 'weight' => '3', ),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		if ($direction === 'down') {
+			return true;
+		}
+
+		//UserAttributeSettingのdata_type_key変更
+		$update = array(
+			'data_type_key' => '\'select\''
+		);
+		$conditions = array(
+			'data_type_key' => 'language',
+		);
+		$UserAttributeSetting = $this->generateModel('UserAttributeSetting');
+		if (! $UserAttributeSetting->updateAll($update, $conditions)) {
+			return false;
+		}
+
+		$UserAttribute = $this->generateModel('UserAttribute');
+		$userAttributes = $UserAttribute->find('list', array(
+			'recursive' => -1,
+			'fields' => array('language_id', 'id'),
+			'conditions' => array('key' => 'language')
+		));
+
+		foreach ($this->records['UserAttributeChoice'] as $i => $record) {
+			$record['user_attribute_id'] = Hash::get($userAttributes, $record['language_id']);
+			$this->records['UserAttributeChoice'][$i] = $record;
+		}
+
+		foreach ($this->records as $model => $records) {
+			if (!$this->updateRecords($model, $records)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
---
+++ Config/Migration/1434983282_add_language_records.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * ユーザ項目に言語追加 migration
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsMigration', 'NetCommons.Config/Migration');
+
+/**
+ * ユーザ項目に言語追加 migration
+ *
+ * @package NetCommons\UserAttributes\Config\Migration
+ */
+class AddLanguageRecords extends NetCommonsMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'add_language_records';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(),
+		'down' => array(),
+	);
+
+/**
+ * Insert records
+ *
+ * @var array $migration
+ */
+	public $records = array(
+		'UserAttributeSetting' => array(
+			array(
+				'user_attribute_key' => 'language',
+				'data_type_key' => 'language',
+				'row' => '1',
+				'col' => '1',
+				'weight' => '5',
+				'required' => '1',
+				'display' => '1',
+				'only_administrator_readable' => '0',
+				'only_administrator_editable' => '0',
+				'is_system' => '1',
+				'display_label' => '1',
+				'display_search_result' => '0',
+				'self_public_setting' => '0',
+				'self_email_setting' => '0',
+				'is_multilingualization' => '0',
+				'auto_regist_display' => null,
+				'auto_regist_weight' => '6',
+			),
+		),
+		'UserAttribute' => array(
+			//日本語
+			array('language_id' => '2', 'key' => 'language', 'name' => '言語', ),
+			//英語
+			array('language_id' => '1', 'key' => 'language', 'name' => 'Language', ),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		if ($direction === 'down') {
+			return true;
+		}
+
+		//weightの更新
+		$update = array(
+			'weight' => 'weight + 1'
+		);
+		$conditions = array(
+			'row' => $this->records['UserAttributeSetting'][0]['row'],
+			'col' => $this->records['UserAttributeSetting'][0]['col'],
+			'weight >=' => $this->records['UserAttributeSetting'][0]['weight'],
+		);
+		$Model = $this->generateModel('UserAttributeSetting');
+		if (! $Model->updateAll($update, $conditions)) {
+			return false;
+		}
+
+		//auto_regist_weightの更新
+		$update = array(
+			'auto_regist_weight' => 'auto_regist_weight + 1'
+		);
+		$conditions = array(
+			'auto_regist_weight >=' => $this->records['UserAttributeSetting'][0]['auto_regist_weight'],
+			'auto_regist_weight <' => '9999',
+		);
+		$Model = $this->generateModel('UserAttributeSetting');
+		if (! $Model->updateAll($update, $conditions)) {
+			return false;
+		}
+
+		foreach ($this->records as $model => $records) {
+			if (!$this->updateRecords($model, $records)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
