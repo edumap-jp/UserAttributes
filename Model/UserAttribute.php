b4f5a3895de7c909b14c46c9a8e78115c73472b9
s-nakajima <nakajimashouhei@gmail.com>
2017-05-08 19:04:44 +0900

    入会・退会の「自動登録時で入力させる項目」の設定のemailは必須とする https://github.com/NetCommons3/NetCommons3/issues/604

--- Model/UserAttribute.php
+++ Model/UserAttribute.php
@@ -45,6 +45,13 @@ class UserAttribute extends UserAttributesAppModel {
 	const LOGIN_ID_FIELD = 'username';
 
 /**
+ * eメールフィールド
+ *
+ * @var const
+ */
+	const EMAIL_FIELD = 'email';
+
+/**
  * パスワードフィールド
  *
  * @var const
@@ -244,6 +251,10 @@ class UserAttribute extends UserAttributesAppModel {
 				//ログインIDとパスワードは、強制的に編集可にする。
 				$result['UserAttributesRole']['self_editable'] = true;
 				$result['UserAttributeSetting']['only_administrator_editable'] = false;
+
+			} elseif ($userAttribute['UserAttribute']['key'] === 'email') {
+				//emailは必須とする
+				$result['UserAttributeSetting']['required'] = true;
 
 			} elseif ($userAttribute['UserAttribute']['key'] === 'role_key') {
 				//権限の設定
